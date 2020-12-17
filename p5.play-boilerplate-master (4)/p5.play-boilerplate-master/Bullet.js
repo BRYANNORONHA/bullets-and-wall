a39f8db7e3a4306d2ad526b07cf476a1a97c7a3f
BRYANNORONHA <71547748+BRYANNORONHA@users.noreply.github.com>
2020-12-17 16:54:20 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master (4)/p5.play-boilerplate-master/Bullet.js
@@ -0,0 +1,17 @@
+class BaseClass{
+    constructor(x, y, width, height, angle) }
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        if (hasCollided(bullet,wall)){
+            bullet.velocitX = 0
+            var damage = 0.5 * weight * speed * speed(thickness * thickness * thickness);
+          if (damage>10){
+           wall.shapeColor = color(255,0,0)
+          }
+          if (damage<10){
+           wall.shapeColor = color(0,255,0)
+          }
+          }
